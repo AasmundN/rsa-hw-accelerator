c68d2bcf00555a46e45afb1b4557e1b3cba4329f
Adrian Niazi <adrian.izi@protonmail.com>
2024-10-26 14:00:24 +0200

    modmul top level module signals

---
+++ source/modmul/modmul.vhd
@@ -0,0 +1,39 @@
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
+
+entity modmul is
+  generic (
+    bit_width : integer := 256
+  );
+  port (
+    -----------------------------------------------------------------------------
+    -- Modulus (n) of the modulo operation
+    -----------------------------------------------------------------------------
+    modulus : in    std_logic_vector(bit_width - 1 downto 0);
+
+    -----------------------------------------------------------------------------
+    -- Operands of Montgomery product
+    -----------------------------------------------------------------------------
+    operand_a : in    std_logic_vector(bit_width - 1 downto 0);
+    operand_b : in    std_logic_vector(bit_width - 1 downto 0);
+
+    -----------------------------------------------------------------------------
+    -- Result of calculation
+    -----------------------------------------------------------------------------
+    result : out   std_logic_vector(bit_width - 1 downto 0)
+
+    -----------------------------------------------------------------------------
+    -- control signals
+    -----------------------------------------------------------------------------
+    enable : in std_logic;
+    output_valid : out std_logic
+
+  );
+end entity modmul;
+
+architecture rtl of modmul is
+
+begin
+
+end architecture rtl;
