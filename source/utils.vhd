7deecc2b52cf7c9087731e5f891f182975509627
Aasmund Nørsett <aasmund.norsett@gmail.com>
2024-10-26 12:58:28 +0200

    feat: add utils package

---
+++ source/utils.vhd
@@ -0,0 +1,13 @@
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
+
+package utils is
+
+  type alu_opcode_t is (pass, add, sub);
+
+end package utils;
+
+package body utils is
+
+end package body utils;
