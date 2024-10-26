9eee11a19465cd33ecea0aca2f83d5490b10d65e
Mori Rosland MAC <moriar@stud.ntnu.no>
2024-10-26 15:40:37 +0200

    feat: monpro control processes

--- source/monpro/monpro_control.vhd
+++ source/monpro/monpro_control.vhd
@@ -2,6 +2,9 @@ library ieee;
   use ieee.std_logic_1164.all;
   use ieee.numeric_std.all;
 
+  -- include utils package
+  use work.utils.all;
+
 entity monpro_control is
   port (
     -- Clocks and reset
@@ -24,7 +27,7 @@ entity monpro_control is
 
     -- Data control
     out_valid      : out   std_logic;
-    opcode         : out   std_logic_vector(1 downto 0);
+    opcode         : out   alu_opcode_t;
     alu_a_sel      : out   std_logic;
     alu_b_sel      : out   std_logic;
     incr_i_counter : out   std_logic
@@ -34,19 +37,82 @@ end entity monpro_control;
 architecture rtl of monpro_control is
 
   type state_type is (
-    -- Wait and initialise states
-    idle, start,
-
-    -- Compute states
-    add_b, add_n, shift, comp,
-
-    -- Save data state
-    save,
-
-    -- Valid state
-    valid
+    idle, start,               -- Wait and initialise states
+    add_b, add_n, shift, comp, -- Compute states
+    save,                      -- Save data state
+    valid                      -- Valid state
   );
 
+  signal state, state_next : state_type;
+
 begin
+
+  main_state_process : process (state) is
+  begin
+
+    -- TODO: Sett defaults
+    monpro_en          <= '0';
+    outreg_en          <= '0';
+    shift_reg_en       <= '0';
+    shift_reg_shift_en <= '0';
+
+    out_valid      <= '0';
+    opcode         <= pass;
+    alu_a_sel      <= '0';
+    alu_b_sel      <= '0';
+    incr_i_counter <= '0';
+
+    case(state) is
+
+      when idle =>
+
+        out_valid <= '1';
+
+      when start =>
+
+        out_valid <= '1';
+
+      when add_b =>
+
+        out_valid <= '1';
+
+      when add_n =>
+
+        out_valid <= '1';
+
+      when shift =>
+
+        out_valid <= '1';
+
+      when comp =>
+
+        out_valid <= '1';
+
+      when save =>
+
+        out_valid <= '1';
+
+      when valid =>
+
+        out_valid <= '1';
+
+      when others =>
+
+        out_valid <= '1';
+
+    end case;
+
+  end process main_state_process;
+
+  update_state : process (reset, clk) is
+  begin
+
+    if (reset = '0') then
+      state <= idle;
+    elsif (rising_edge(clk)) then
+      state <= state_next;
+    end if;
+
+  end process update_state;
 
 end architecture rtl;
