5fcf1e716e053b147008decde29d08e17611a7db
Mori Adrian Rosland <mori@live.no>
2024-10-28 20:45:30 +0100

    feat: rsa core control main process

--- source/rsa_core_control.vhd
+++ source/rsa_core_control.vhd
@@ -1,11 +1,14 @@
 library ieee;
-    use ieee.std_logic_1164.all;
-    use ieee.numeric_std.all;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
+
+  -- include utils package
+  use work.utils.all;
 
 
 entity rsa_core_control is
     port(
-        clk : out std_logic;
+        clk : in std_logic;
         reset : in std_logic;
 
         msgin_ready : out std_logic;
@@ -13,7 +16,7 @@ entity rsa_core_control is
         msgin_last : in std_logic;
         
         msgout_ready : in std_logic;
-        msout_valid : out std_logic;
+        msgout_valid : out std_logic;
         msgout_last : out std_logic;
 
         modmul_valid : in std_logic;
@@ -30,7 +33,7 @@ architecture rtl of rsa_core_control is
         result_out -- "out" is a keyword and cannot be used (ref state_diagrams.xlsx)
     ) ;
 
-    signal state, next_state : state_type;
+    signal state, state_next : state_type;
 
     signal modmul_en : std_logic;
     signal modexp_out_ready : std_logic;
@@ -40,9 +43,98 @@ architecture rtl of rsa_core_control is
 
 
 begin
-    next_state <= rdy;
-    modmul_en <= '0';
-    modmul_valid <= '0';
-    modexp_in_ready <= '0';
+    main_process : process (clk, reset, state) is
+        begin
+            state_next <= rdy;
+
+            msgin_ready <= '0';
+            msgout_valid <= '0';
+
+            modmul_en <= '0';
+            modexp_out_ready <= '0';
+            modexp_in_valid <= '0';
+
+            in_reg_en <= '0';
+            m_reg_en <= '0';
+            last_reg_en <= '0';
+            out_reg_en <= '0';
+
+            -- TODO: Reset = 0 -> state = ready
+            case(state) is
+                when rdy =>
+                    msgin_ready <= '1';
+                    msgout_valid <= '0';
+                    in_reg_en <= '1';
+                    last_reg_en <= '1';
+
+                    if (msgin_valid = '0') then
+                       state_next <= rdy;
+                    else
+                        state_next <= modmul;
+                    end if;
+
+                when modmul =>
+                    msgin_ready <= '0';
+                    in_reg_en <= '0';
+                    last_reg_en <= '0';
+                    modmul_en <= '1';
+
+                    if (modmul_valid = '0') then
+                       state_next <= modmul;
+                    else
+                        state_next <= save_modmul;
+                    end if;
+
+                when save_modmul =>
+                    m_reg_en <= '1';
+                    modmul_en <= '0';
+                    state_next <= modexp_in;
+
+                when modexp_in =>
+                    modexp_in_valid <= '1';
+
+                    if (modexp_in_ready = '0') then
+                       state_next <= modexp_in;
+                    else
+                        state_next <= modexp_out;
+                    end if;
+                when modexp_out =>
+                    out_reg_en <= '1';
+                    modexp_out_ready <= '1';
+
+                    if (modexp_out_valid = '0') then
+                       state_next <= modexp_out;
+                    else
+                        state_next <= result_out;
+                    end if;
+                when result_out =>
+                    msgout_valid <= '1';
+                    
+                    if (msgout_ready = '0') then
+                       state_next <= result_out;
+                    else
+                        state_next <= rdy;
+                    end if;
+
+                when others =>
+                    state_next <= rdy;
+
+                    msgin_ready <= '0';
+                    msgout_valid <= '0';
+
+                    modmul_en <= '0';
+                    modexp_out_ready <= '0';
+                    modexp_in_valid <= '0';
+
+                    in_reg_en <= '0';
+                    m_reg_en <= '0';
+                    last_reg_en <= '0';
+                    out_reg_en <= '0';
+
+            end case;
+        end process main_process;
+
+
+
 
 end architecture;
