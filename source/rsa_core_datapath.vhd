9612fc1c878b2b0af245233c719d501bcd1210e1
Mori Adrian Rosland <mori@live.no>
2024-10-28 19:55:28 +0100

    feat: rsa_core scaffold

---
+++ source/rsa_core_datapath.vhd
@@ -0,0 +1,20 @@
+library ieee;
+    use ieee.std_logic_1164.all;
+    use ieee.numeric_std.all;
+
+
+entity rsa_core_datapath is
+    port (
+        clk   : in std_logic;
+        reset : in std_logic;
+        
+    );
+end entity;
+
+architecture rtl of rsa_core_datapath is
+
+begin
+
+    
+
+end architecture;
