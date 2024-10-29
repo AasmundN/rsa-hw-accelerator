444905567a7bcce628e2f907e7318de58f7a5591
Adrian Niazi <adrian.izi@protonmail.com>
2024-10-29 15:20:29 +0100

    fix: use module msb_bitscanner in modexp

--- source/source.mk
+++ source/source.mk
@@ -2,5 +2,5 @@ alu: utils
 monpro: monpro_datapath monpro_control
 monpro_datapath: alu mux_2to1
 modexp: modexp_datapath
-modexp_datapath: monpro mux_3to1 mux_2to1
+modexp_datapath: monpro mux_3to1 mux_2to1 msb_bitscanner
 rsa_core: rsa_core_control
