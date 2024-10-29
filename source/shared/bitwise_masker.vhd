430bdf8ee4a45a94b28df4d08ad9c3a75324d98f
Adrian Niazi <adrian.izi@protonmail.com>
2024-10-29 16:13:51 +0100

    chore: syntax

--- source/shared/bitwise_masker.vhd
+++ source/shared/bitwise_masker.vhd
@@ -4,26 +4,29 @@
 --set_mask_values of 1 results in a bit_width sized mask of 1's
 --set_mask_values of 0 results in a bit_width sized mask of 0's
 ----------------------------------------------------------------------------
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
+
 entity bitwise_masker is
     generic (
-        bit_width : integer = 256;
+        bit_width : integer := 256
     );
     port (
         signal_in : in  std_logic_vector(bit_width - 1 downto 0);
         set_mask_values : in std_logic;
 
-        signal_out : out std_logic_vector(bit_width - 1 downto )
+        signal_out : out std_logic_vector(bit_width - 1 downto 0)
     );
-end entity;
+end entity bitwise_masker;
 
 
 architecture rtl of bitwise_masker is
 begin
 
-    process (signal_bit, set_mask_values)
+    process (signal_in, set_mask_values)
     begin
         for i in 0 to bit_width - 1 loop
-        begin
             signal_out(i) <= signal_in(i) and set_mask_values;
         end loop;
     end process;
